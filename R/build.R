e217b9043453e40c34bdbcd781325154d8867adc
Devin Pastoor <devin.pastoor@gmail.com>
2018-11-09 17:56:43 -0500

    build from package directory

--- R/build.R
+++ R/build.R
@@ -19,10 +19,11 @@ modify_desc <- function(.d, meta, loc, overwrite = TRUE) {
 
 
 #' build various versions of a package
-#' @param .pkg path to package
+#' @param .pkgdir path to package
 #' @param repository repository name being built for
 #' @param origin package source
 #' @param supplement_version whether to add additional version info (unix timestamp)
+#' @param overwrite overwrite fields already present when adding fields
 #' @details
 #' supplementing version can be done whenever a build occurs that does not
 #' correspond to a formal release/tag. This will automatically add information
@@ -30,38 +31,39 @@ modify_desc <- function(.d, meta, loc, overwrite = TRUE) {
 #' with a unix timestamp that corresponds to the last git hash (if present) or
 #' the current system time, if git is not present.
 #' @export
-build_pkg <- function(.pkg = ".",
+build_pkg <- function(.pkgdir = ".",
                       types = c("source", "binary"),
                        repository = NULL,
                        origin = NULL,
                        supplement_version = FALSE,
                        overwrite = TRUE) {
-  pkg_desc <- file.path(.pkg, "DESCRIPTION")
-  d__ <- desc::desc(pkg_desc)
-  meta <- list(Repository = repository, Origin = origin)
-  if (supplement_version) {
-    hs <- hashstamp(.pkg)
-    version <- d__$get_version()
-    hs$Version <- sprintf("%s.%s", version, hs$timestamp)
-    meta <- modifyList(meta, hs)
-  }
-  modify_desc(d__, drop_nulls(meta), loc = pkg_desc, overwrite = overwrite)
-  on.exit({
-    # d__ should be unchanged
-    d__$write(pkg_desc)
-  }, add = TRUE)
-  result <- tryCatch({
-    balls <- lapply(types, function(.t) {
-     switch(.t,
-            source = pkgbuild::build(.pkg),
-            binary = pkgbuild::build(.pkg, binary = TRUE),
-            stop(sprintf("cannot build type %s", .t)))
+  withr::with_dir(.pkgdir, {
+    pkg_desc <- file.path(.pkgdir, "DESCRIPTION")
+    d__ <- desc::desc(pkg_desc)
+    meta <- list(Repository = repository, Origin = origin)
+    if (supplement_version) {
+      hs <- hashstamp()
+      version <- d__$get_version()
+      hs$Version <- sprintf("%s.%s", version, hs$timestamp)
+      meta <- modifyList(meta, hs)
+    }
+    modify_desc(d__, drop_nulls(meta), loc = pkg_desc, overwrite = overwrite)
+    on.exit({
+      # d__ should be unchanged
+      d__$write(pkg_desc)
+    }, add = TRUE)
+    result <- tryCatch({
+      tballs <- lapply(types, function(.t) {
+        switch(.t,
+               source = pkgbuild::build(.pkgdir),
+               binary = pkgbuild::build(.pkgdir, binary = TRUE),
+               stop(sprintf("cannot build type %s", .t)))
+      })
+      setNames(tballs, types)
+    }, error = function(e) {
+      message("building failed", e)
+      e
     })
-    setNames(balls, types)
-  }, error = function(e) {
-    message("building failed", e)
-    e
+    return(result)
   })
-
-  return(result)
 }
