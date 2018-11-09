bdcff994a63589ffacabfc30226ea8c52451803c
Devin Pastoor <devin.pastoor@gmail.com>
2018-11-09 15:44:05 -0500

    capture the system commands and return as list
    without stdout = TRUE, just prints the system2 calls to console, and returns the exit code,
    so timestamp/hash were coming out to 0

--- R/hashstamp.R
+++ R/hashstamp.R
@@ -5,11 +5,12 @@ hashstamp <- function(path) {
   }, add = TRUE)
   setwd(path)
   metadata <- tryCatch({
-    timestamp <- system2("git", args = c("show", "-s", "--format=%ct"))
-    hash <- system2("git", args = c("rev-parse", "HEAD"))
-  }, function(e) {
+    timestamp <- as.numeric(system2("git", args = c("show", "-s", "--format=%ct"), stdout = TRUE))
+    hash <- system2("git", args = c("rev-parse", "HEAD"), stdout = TRUE)
+    list(timestamp = timestamp, hash = hash)
+  }, error = function(e) {
     warning("no git detected on system, using system time for timestamp")
-    return(timestamp = as.numeric(Sys.time()))
+    return(list(timestamp = as.numeric(Sys.time())))
   })
   return(metadata)
 }
