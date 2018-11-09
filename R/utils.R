d40ac639898a821f10d3e420900427dc759a6e3a
Devin Pastoor <devin.pastoor@gmail.com>
2018-11-09 17:23:40 -0500

    shell helper from a given directory

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,25 @@ drop_nulls <- function(.x) {
   .x[sapply(.x, is.null)] <- NULL
   .x
 }
+
+sh <- function(cmd, args, stdout = TRUE, ..., .dir = getwd()) {
+  owd <- getwd()
+  if (.dir != owd) {
+    setwd(.dir)
+    on.exit({
+      setwd(owd)
+    }, add = TRUE)
+  }
+  tryCatch({
+    system2(cmd, args, stdout = stdout, ...)
+  }, error = function(e) {
+    e
+  })
+}
+
+if_error <- function(.x, when_error = NULL) {
+  if (inherits("simpleError")) {
+    return(when_error)
+  }
+  return(.x)
+}
