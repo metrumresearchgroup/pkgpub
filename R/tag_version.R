f2b06035826feb6910cde51db61e2a5b71950183
Devin Pastoor <devinp@metrumrg.com>
2020-03-22 15:34:23 -0400

    feat: add tagging helper

---
+++ R/tag_version.R
@@ -0,0 +1,22 @@
+#' add a git tag based on the pkg version
+#' @param .dir dir of repo
+#' @param .dirty whether to tag given the repo is in a dirty state
+#' @param .fetch run a git fetch first to get tags
+#' @export
+tag_version <- function(.dir = getwd(), .dirty = FALSE, .fetch = TRUE) {
+  if (.fetch) {
+    gert::git_fetch()
+  }
+  pkg_version <- as.data.frame(read.dcf(file.path(.dir,"DESCRIPTION"),fields = "Version"))$Version
+  status <- gert::git_status()
+  if (nrow(status) && !.dirty) {
+    stop("not tagging as currently detected the following dirty files: ",
+         glue::glue_collapse(status$file)
+    )
+  }
+  tag_list <- gert::git_tag_list()
+  if (nrow(tag_list) && pkg_version %in% tag_list$name) {
+    stop("tag already exists in repo")
+  }
+  gert::git_tag_create(pkg_version)
+}
