4bc4356af35c8b4e5aaf1f4465aebddf8056f7f8
Devin Pastoor <devinp@metrumrg.com>
2020-06-07 18:12:56 -0400

    add: functionality to skip pipeline steps for tagging for dev packages

---
+++ R/exit.R
@@ -0,0 +1,17 @@
+exit_with_statuscode <- function(status) {
+  quit(status = status, save = 'no', runLast = FALSE)
+}
+
+skip_remaining_drone_ci_steps <- function() {
+  # https://discourse.drone.io/t/how-to-exit-a-pipeline-early-without-failing/3951
+  exit_with_statuscode(78)
+}
+
+#' skip remaining drone CI steps if tag is dev tag
+#' @export
+skip_remaining_if_dev_tag <- function() {
+  if (is_dev_tag()) {
+    skip_remaining_ci_steps()
+  }
+  exit_with_statuscode(0)
+}
--- R/tags.R
+++ R/tags.R
@@ -5,3 +5,10 @@ head_tag <- function() {
     if_error(character(0))
 }
 
+is_dev_tag <- function() {
+  ctag <- current_commit_tag()
+  # given 0.1.2.9000 the 4 will be 9000
+  # given 0.1.2 the 4 will be NA
+  dev_version <- pv[1, 4]
+  return(is.na(dev_version))
+}
